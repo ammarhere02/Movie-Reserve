7a06b874cff02f05da847db52dfa1e70095039cf
Ammar Khan <ammarkhancloud@icloud.com>
2025-01-06 20:06:36 +0500

    Initial Commit

---
+++ middlewares/authorization.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+const {signIn} = require("../controllers/authentication");
+dotenv.config();
+const secretKey = process.env.SECRETKEY;
+
+
+const authorization = async(req, res) => {
+
+    req.user = null;
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+
+        return res.status(401).json({message:"Failed to get headers"});
+    }
+    const token = authHeader.split(" ")[1];
+    if(!token){
+        return res.status(401).json({message:"No token provided"});
+    }
+    const verifyToken = jwt.verify(token , secretKey);
+    if(!verifyToken){
+        return res.status(401).json({message:"Your credentials does'ot match"});
+    }
+    console.log(verifyToken);
+    return res.status(200).json({message:"Authorization completed successfully"});
+
+}
+
+
+
+module.exports = {authorization}
